6a64beac885687cef386a36db74a147adc11a093
luidmilab <42977343+luidmilab@users.noreply.github.com>
2018-09-16 22:35:10 +0300

    Add files via upload

---
+++ lesson_9/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+console.log ('"Башлий Людмила"');
+
+const a = 19;
+const b = 8;
+
+console.log (b > a ? b : a);
+console.log (b > a ? a : b);
+console.log (a + b);
+console.log (a % b);
+if (a - b > 10) {
+	console.log(a + 10);
+} else if (a < b){
+	console.log(9);
+} else {
+	console.log(b);
+}

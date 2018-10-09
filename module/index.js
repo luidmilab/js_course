ea382e971b9f47f2eb363d1663566667262204b6
luidmilab <42977343+luidmilab@users.noreply.github.com>
2018-10-09 19:32:32 +0300

    Add files via upload

---
+++ module/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//1
+let a;
+function itsMe(a) {
+		return console.log(a!==''? ("It's me, " + a) : ("It's me, luidmilab") ); 
+
+}
+
+//2
+const numOne;
+const numTwo;
+
+function compare(numOne, numTwo){
+	
+	return ;
+}

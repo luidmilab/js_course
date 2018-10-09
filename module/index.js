cde93cfe404cf63c0988d08867ceec7e0fe6d1c2
luidmilab <42977343+luidmilab@users.noreply.github.com>
2018-10-09 20:25:00 +0300

    Add files via upload

--- module/index.js
+++ module/index.js
@@ -14,25 +14,46 @@ let numTwo;
 function compare(numOne, numTwo){
 	let res;
 	if (numOne > numTwo) {
-		console.log ('numOne > numTwo');
+		res = ('numOne > numTwo');
 		
 
 	}	else if (numOne < numTwo) {
-		console.log ('umOne < numTwo');
+		res =  ('umOne < numTwo');
 		
 
 	} 	else if (numOne !== numTwo) {
-		console.log ('numOne == numTwo');
+		res =  ('numOne == numTwo');
 		
 
 	} 	else if (typeof (numOne)!== 'number' || typeof(numTwo)!== 'number') {
-		console.log ('НЕ ЧИСЛО');
+		res = ('НЕ ЧИСЛО');
 	
 	}
+	return res;
 
 	}
 
 	//3
-	
+	//Написать функцию “row”, которая принимает 4 аргумента - 3 числа  и строку которая может быть ‘>’ или ‘<’
+//Она должна вернуть строку в которой числа стоят в порядке убывания или возрастания в зависимости от аргумента строки.
+
+//Пример:
+//row(3, -4, 5, '<') // вернет "-4 < 3 < 5"
+//row(3, -4, 5, '>') // вернет "5 > 3 > -4"
+
+let b;
+let c;
+let d;
+let mark = '';
+
+function row(a, b, c, mark){
+	let result;
+	if (mark === '>'){
+		resalt = ()
+
+	}
+
+}
+
 			
 	
